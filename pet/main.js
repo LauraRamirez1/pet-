648e8f81d4b570bcd6368cba808fe560e2cf1ad0
Jacqueline Galimany <42303655+jgalsku@users.noreply.github.com>
2023-03-28 07:01:05 -0700

    password y email validations

--- pet/main.js
+++ pet/main.js
@@ -16,6 +16,9 @@ function clearInputError(inputElement) {
     inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
 }
 
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.querySelector("#login");
     const createAccountForm = document.querySelector("#createAccount");
@@ -34,21 +37,67 @@ document.addEventListener("DOMContentLoaded", () => {
 
     loginForm.addEventListener("submit", e => {
         e.preventDefault();
-
+        
         // Perform your AJAX/Fetch login
 
-        setFormMessage(loginForm, "error", "Invalid username/password combination");
+        setFormMessage(loginForm, "error", "Combinación inválida de Usuario/Contraseña");
+           
+    
     });
 
     document.querySelectorAll(".form__input").forEach(inputElement => {
         inputElement.addEventListener("blur", e => {
-            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
-                setInputError(inputElement, "Username must be at least 10 characters in length");
+            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 5) {
+                setInputError(inputElement, "Nombre de usario debe tener al menos 5 caracteres");
             }
+
+            /* Validaciones contraseña en creación de cuenta */
+
+            if (e.target.id === "signupPassword" && e.target.value.length > 0 && e.target.value.length < 10) {
+                setInputError(inputElement, "Contraseña debe tener al menos 10 caracteres");
+            }
+
+            re = /[0-9]/;
+            if (e.target.id === "signupPassword" && !re.test(e.target.value)) {
+                setInputError(inputElement, "Contraseña debe tener al menos un número");
+            }
+
+            re = /[a-z]/;
+            if (e.target.id === "signupPassword" && !re.test(e.target.value)) {
+                setInputError(inputElement, "Contraseña debe contener al menos una letra minúscula");
+            }
+
+            re = /[A-Z]/;
+            if (e.target.id === "signupPassword" && !re.test(e.target.value)) {
+                setInputError(inputElement, "Contraseña debe contener al menos una letra mayúscula");
+            }
+
+            re = /[_?!<>]/;
+            if (e.target.id === "signupPassword" && !re.test(e.target.value)) {
+                setInputError(inputElement, "Contraseña debe contener al menos un caracter especial (_?!<>)");
+            }
+            
+            /* Validaciones email en creación de cuenta */
+
+
+            mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            if (e.target.id === "signupEmail" && !mailformat.test(e.target.value)) {
+                setInputError(inputElement, "asdasdas");
+            }
+           
+            
+
+
+
         });
+
 
         inputElement.addEventListener("input", e => {
             clearInputError(inputElement);
         });
+
+
     });
+
+
 });
